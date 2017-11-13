ca05a4236175a9aaf467028d517ed49fe9713217
chris <dwatow@gmail.com>
2017-11-13 14:22:33 +0800

    modify doc

--- doc/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-11-12T06:53:46.254Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
